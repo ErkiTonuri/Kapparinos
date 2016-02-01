a227dedc311c53e400a54e21add236c10ec59c5c
Erki Tõnuri <erki.tnuri@gmail.com>
2016-02-01 09:44:51 +0200

    Tegin sprite mängu faili

---
+++ spriterinos.py
@@ -0,0 +1,52 @@
+import pygame
+import random
+
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface([width, height])
+        self.image.fill(WHITE)
+        self.image.set_colorkey(WHITE)
+        # Draw the ellipse
+        pygame.draw.ellipse(self.image, color, [0, 0, width, height])
+
+pygame.init()
+
+size = (1000, 700)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("My Game")
+
+my_list = []
+
+clock = pygame.time.Clock()
+
+done = False
+
+for i in range(500):
+    my_object = Block()
+    my_list.append(my_object)
+
+
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    screen.fill(WHITE)
+    p = len(my_list)
+    for i in range(p):
+        my_object = my_list[i]
+        my_object.move()
+        my_object.draw()
+
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
